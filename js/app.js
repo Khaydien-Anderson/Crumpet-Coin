2acf0d0a998cf1828cd81c53e480f688c64af10c
K Andy <83248167+Khaydien-Anderson@users.noreply.github.com>
2021-12-01 21:14:38 +0000

    arrow added and onclick form submit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+///ARROW FLASH DISAPEARS AFTER 3 SECONDS
+document.addEventListener('DOMContentLoaded', () => {
+        setTimeout(() => {
+            
+            const arrow = document.getElementById('arrow')
+            arrow.style.display = 'none'
+        }, 3000); 
+})
+
+// FORM FILLED OUT CORRECT OR INCORRECTLY
+
+const submitButton = document.querySelector('.submitButton');
+const emailField = document.querySelector('.emailField');
+const message = document.getElementById('emailHelp');
+
+function formSubmit() {
+    
+    message.innerHTML = '<p id="thanks">Thank you! Welcome to the Crumpet Community! #CMPT</p>'
+    
+}
