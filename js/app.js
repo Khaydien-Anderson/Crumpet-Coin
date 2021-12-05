418583915c70834208699a4326a30a93a2e8dbe4
K Andy <83248167+Khaydien-Anderson@users.noreply.github.com>
2021-12-05 16:12:41 +0000

    domain connected & token addy added w/ email

--- js/app.js
+++ js/app.js
@@ -4,17 +4,46 @@ document.addEventListener('DOMContentLoaded', () => {
             
             const arrow = document.getElementById('arrow')
             arrow.style.display = 'none'
-        }, 3000); 
+        }, 2000); 
 })
 
 // FORM FILLED OUT CORRECT OR INCORRECTLY
 
 const submitButton = document.querySelector('.submitButton');
-const emailField = document.querySelector('.emailField');
+const emailField = document.querySelector('#exampleInputEmail1');
 const message = document.getElementById('emailHelp');
 
 // function formSubmit() {
     
-//     message.innerHTML = '<p id="thanks">Thank you! Welcome to the Crumpet Community! #CMPT</p>'
+    
     
 // }
+
+////SEND MAIL 
+
+function validateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById('exampleInputEmail1').value))
+  {
+   message.innerHTML = '<p id="thanks">Thank You! Welcome to the CMPT community!</p>'
+    sendMail();
+  } else {
+  message.innerHTML = '<p id="sorry">Please enter a valid email address.</p>'}
+    
+}
+
+function sendMail(params) {
+ 
+    var tempParams = {
+        from_name: document.getElementById('exampleInputEmail1').value
+
+    };
+
+
+    emailjs.send('service_r5mad6o', 'template_nluwv9p', tempParams, 'user_AAoUSCLmjLy7W5KLaf2zg' )
+    .then(function(res) {
+        console.log('success', res.status);
+    })
+
+}
+
